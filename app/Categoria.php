84398dce2bd6500a65150fc398759de5c0725a82
Cesar Morales <camd.jun@gmail.com>
2017-09-06 22:51:16 -0600

    Configuración del modelo Categoria

---
+++ app/Categoria.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace _200Code_Ventas;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Categoria extends Model
+{
+    protected $table='categoria';
+    protected $primaryKey='id_categoria';
+    public $timestamps=false;
+
+    protected $fillable = [
+        'nombre',
+        'descripcion',
+        'condicion'
+    ];
+
+    protecte $guarded = [];
+}
---
+++ app/Http/Controllers/CategoriaController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace _200Code_Ventas\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CategoriaController extends Controller
+{
+    //
+}
